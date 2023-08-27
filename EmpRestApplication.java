bb0bc7932df9733f5c046d6bb75562b5d93ceb82
Abhishek Kumar <109984370+InfernoTech07@users.noreply.github.com>
2023-08-27 08:01:07 +0530

    Add files via upload

---
+++ EmpRestApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Emp_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpRestApplication.class, args);
+	}
+
+}
